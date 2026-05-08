af5403c075d8dac324959d24d29dff287333fc0a
Thales Oliveira <TOliveiraDev@outlook.com>
2026-05-08 02:06:19 -0300

    feat: refatora script de seed para incluir múltiplos usuários e produtos com enums

--- src/seed.ts
+++ src/seed.ts
@@ -1,4 +1,5 @@
 import { PrismaClient } from "@prisma/client";
+import { PerfilUsuario } from "./enums/PerfilUsuario";
 const prisma = new PrismaClient();
 
 async function main() {
@@ -9,24 +10,71 @@ async function main() {
         }
     });
 
-    const cliente = await prisma.usuario.create({
-        data: {
-            nome: "Thales Oliveira",
-            email: "thales@uninter.com",
-            senha_hash: "hash123",
-            perfil: "CLIENTE"
-        }
+    const usuariosInseridos = await prisma.usuario.createMany({
+        data: [
+            {
+                nome: "Thales Oliveira",
+                email: "thales@uninter.com",
+                senha_hash: "hash123",
+                perfil: PerfilUsuario.CLIENTE
+            },
+            {
+                nome: "Usuario Cliente",
+                email: "cliente@email.com",
+                senha_hash: "cli123",
+                perfil: PerfilUsuario.CLIENTE
+            },
+            {
+                nome: "Atendente Caixa",
+                email: "caixa@raizes.com",
+                senha_hash: "caixa123",
+                perfil: PerfilUsuario.ATENDENTE
+            },
+            {
+                nome: "Cozinha",
+                email: "cozinha@raizes.com",
+                senha_hash: "cozinha123",
+                perfil: PerfilUsuario.COZINHA
+            },
+            {
+                nome: "Gerencia",
+                email: "gerencia@raizes.com",
+                senha_hash: "gerencia123",
+                perfil: PerfilUsuario.GERENTE
+            }
+        ]
+    })
+
+    const produtoInseridos = await prisma.produto.createMany({
+        data: [
+            {
+                nome: "Tapioca de Carne de Sol",
+                descricao: "Deliciosa",
+                preco_base: 15.90
+            },
+            {
+                nome: "Cuscuz Completo",
+                descricao: "Acompanha ovo, calabresa e queijo",
+                preco_base: 12.50
+            },
+            {
+                nome: "Suco de Cajá",
+                descricao: "Copo de 500ml, suco natural",
+                preco_base: 8.00
+            }
+        ]
     });
 
-    const produto = await prisma.produto.create({
-        data: {
-            nome: "Tapioca de Carne de Sol",
-            descricao: "Deliciosa",
-            preco_base: 15.90
-        }
-    });
-
-    console.log(`Seed finalizada. Unidade ID: ${unidade.id} | Cliente ID: ${cliente.id} | Produto ID: ${produto.id}`)
+    console.log(
+        `
+        Seed finalizada. 
+        Foram adicionadas ${unidade.id} unidades, ${produtoInseridos.count} produtos e ${usuariosInseridos.count} usuários.
+        Usuários de ID 1 e 2: CLIENTE
+        Usuário de ID 3: ATENDENTE
+        Usuário de ID 4: COZINHA
+        Usuário de ID 5: GERENTE
+        `
+    );
 }
 
 main()

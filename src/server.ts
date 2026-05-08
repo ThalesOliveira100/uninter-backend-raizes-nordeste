20795035b1e3d986c1b254ed66e4c47012d7183e
Thales Oliveira <TOliveiraDev@outlook.com>
2026-05-08 20:53:19 -0300

    style: remove comentário do código

--- src/server.ts
+++ src/server.ts
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 app.use('/api', routes);
 
-const port = process.env.PORT || 3000; // Usar o dotenv depois.
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Servidor rodando em http://localhost:${port}`);
 });

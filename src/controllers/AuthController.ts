45e4bf457db26a04aba1fc01ed19d895297e40f7
Thales Oliveira <TOliveiraDev@outlook.com>
2026-05-09 00:52:37 -0300

    refactor: removido AppError não usado e passa a utilizar o handler de erros em parte inusada

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,6 +1,5 @@
 import { NextFunction, Request, Response } from "express";
 import { AuthService } from "../services/AuthService";
-import { AppError } from "../errors/AppError";
 
 export class AuthController {
     async login(req: Request, res: Response, next: NextFunction) {
--- src/controllers/PedidoController.ts
+++ src/controllers/PedidoController.ts
@@ -1,9 +1,8 @@
 import { NextFunction, Request, Response } from 'express';
 import { PedidoService } from '../services/PedidoService';
-import { AppError } from '../errors/AppError';
 
 export class PedidoController {
-    async criar(req: Request, res: Response) {
+    async criar(req: Request, res: Response, next: NextFunction) {
         try {
             const pedidoService = new PedidoService();
             const novoPedido = await pedidoService.criar(req.body);
@@ -11,22 +10,7 @@ export class PedidoController {
             return res.status(201).json(novoPedido);
 
         } catch (error: any) {
-            if (error instanceof AppError) {
-                return res.status(error.statusCode).json({
-                    error: error.errorCode,
-                    message: error.message,
-                    details: error.details,
-                    timestamp: new Date().toISOString(),
-                    path: req.originalUrl
-                });
-            };
-
-            return res.status(500).json({
-                error: "ERRO_INTERNO",
-                message: "Falha ao criar o pedido no banco de dados.",
-                timestamp: new Date().toISOString(),
-                path: req.originalUrl
-            });
+            next(error);
         }
     }
 

6a35983f248dbce55f13eaf7a2dd6566ab9e4955
Thales Oliveira <TOliveiraDev@outlook.com>
2026-05-08 23:31:30 -0300

    feat: implementa controle de autenticação com login e gerenciamento de erros

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { pedidoRoutes } from "./pedidoRoutes";
-// import { authRoutes } from './authRoutes';
+import { authRoutes } from './authRoutes';
 // import { produtoRoutes } from './produtoRoutes';
 
 const routes = Router();
@@ -10,8 +10,9 @@ routes.get('/ping', (req, res) => {
     res.json({ message: 'API Raízes do Nordeste rodando com sucesso!' });
 });
 
-routes.use('/pedidos', pedidoRoutes);
-// routes.use('/auth', authRoutes);
+routes
+    .use('/pedidos', pedidoRoutes)
+    .use('/auth', authRoutes)
 // routes.use('/produtos', produtoRoutes);
 
 export { routes };
---
+++ src/services/AuthService.ts
@@ -0,0 +1,61 @@
+import bcrypt from "bcryptjs";
+import jwt from 'jsonwebtoken';
+import { UsuarioRepository } from "../repositories/UsuarioRepository";
+import { ErrorCredenciaisInvalidas } from "../errors/ErrorCredenciaisInvalidas";
+import { ErrorDadosIncompletos } from "../errors/ErrorDadosIncompletos";
+
+export class AuthService {
+    private usuarioRepository: UsuarioRepository;
+
+    constructor() {
+        this.usuarioRepository = new UsuarioRepository;        
+    }
+
+    async login(dados: any) {
+        const { email, senha } = dados;
+
+        if (!email || !senha) {
+            throw new ErrorDadosIncompletos([
+                { field: "email, senha", issue: "Ausentes" }
+            ]);
+        };
+
+        const usuario = await this.usuarioRepository.buscarPorEmail(email);
+        if (!usuario) {
+            throw new ErrorCredenciaisInvalidas();
+        };
+
+        const senhaValida = await bcrypt.compare(senha, usuario.senha_hash);
+
+        // *Gambiara de testes*: Como nossa seed atual inseriu senhas sem hash (ex: "hash123"), 
+        // deixamos uma validação temporária para o texto puro também. Futuramente ajustaremos a seed!
+        if (!senhaValida && senha !== usuario.senha_hash) {
+            throw new ErrorCredenciaisInvalidas();
+        };
+
+        const secret = process.env.JWT_SECRET || 'super_secreto_desenvolvimento';
+        const expiresIn = '1d';
+
+        const token = jwt.sign (
+            { id: usuario.id, perfil: usuario.perfil },
+            secret,
+            { expiresIn }
+        );
+
+        return {
+            accessToken: token,
+            tokenType: "Bearer",
+            expiresIn: 86400,
+            user: this.mapearUsuario(usuario)
+        };
+    };
+
+    mapearUsuario(usuario: any) {
+        return {
+            id: usuario.id,
+            nome: usuario.nome,
+            email: usuario.email,
+            perfil: usuario.perfil
+        }
+    }
+};

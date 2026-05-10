88c70ce2436420cb46b625317d1ab5785d2e8108
Thales Oliveira <TOliveiraDev@outlook.com>
2026-05-10 01:27:18 -0300

    feat: adiciona documentação da API usando Swagger

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,8 @@ import express from 'express';
 import { routes } from './routes/index';
 import "dotenv/config";
 import { errorHandler } from './middlewares/errorHandler';
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from '../swagger.json';
 
 const app = express();
 
@@ -9,7 +11,10 @@ app.use(express.json());
 app.use('/api', routes);
 app.use(errorHandler);
 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Servidor rodando em http://localhost:${port}`);
+    console.log(`Documentação da API em http://localhost:3000/api-docs`);
 });

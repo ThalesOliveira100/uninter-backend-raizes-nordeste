60418f4f5c43a0a3a2713f6d5460d73a101cfde5
Thales Oliveira <TOliveiraDev@outlook.com>
2026-05-10 02:46:28 -0300

    feat: adiciona middleware de autenticação e autorização à rota de listagem de estoque

--- src/routes/estoqueRoutes.ts
+++ src/routes/estoqueRoutes.ts
@@ -1,10 +1,16 @@
 import { Router } from 'express';
 import { EstoqueController } from '../controllers/EstoqueController';
+import { verificarToken } from '../middlewares/authMiddleware';
+import { verificarPermissao } from '../middlewares/roleMiddleware';
+import { PerfilUsuario } from '../enums/PerfilUsuario';
 
 const estoqueRoutes = Router();
 const estoqueController = new EstoqueController();
 
 estoqueRoutes
-    .get('/', estoqueController.listarCardapioDaUnidade.bind(estoqueController));
+    .get('/', 
+        verificarToken,
+        verificarPermissao([PerfilUsuario.GERENTE]),
+        estoqueController.listarCardapioDaUnidade.bind(estoqueController));
 
 export { estoqueRoutes };
